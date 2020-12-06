7651f693fcf8a34173a2cbc688e70f8523a7375f
fruityvegetables <gg.troyer@gmail.com>
2020-12-05 23:19:41 -0600

    added a function to display a dataframe from pandas

--- benfordsApp.py
+++ benfordsApp.py
@@ -4,8 +4,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.DataFrame({'A': [0, 1, 2, 3, 4],
+                   'B': [5, 6, 7, 8, 9],
+                   'C': ['a', 'b', 'c--', 'd', 'e']})
+@app.route('/', methods=("POST", "GET"))   
+def html_table():
+    return render_template('index.html', tables=[df.to_html(classes='data')], titles=df.columns.values)
 
-@app.route('/')
-def hello():
-    return "Hello, World! This is my first Flask app. I plan on using it to assert Benford's law."
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
