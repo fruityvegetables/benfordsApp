b25a951a1db7570fd05deacc6ad3bbe3df33093f
fruityvegetables <gg.troyer@gmail.com>
2020-12-06 20:10:10 -0600

    added a sorted graph to display the distribution of leading digits

--- benfordsProduction.py
+++ benfordsProduction.py
@@ -1,13 +1,15 @@
 import pandas as pd 
 #print('Pandas version', pd.__version__)
+import matplotlib
 import matplotlib.pylab as plt
+import numpy as np
 
-pd.options.display.max_rows = 999
-
+#variables for census data
 census_2009 = pd.read_table('census_2009b')
 census_2009.shape
 census_2009['7_2009'].head()
 
+## function for retrieving first digit
 first_digit_list=[]
 for this_amt in list(census_2009['7_2009']):
    first_digit_list.append(str(this_amt)[0])
@@ -16,3 +18,28 @@ len(first_digit_list)
 
 print(census_2009.shape)
 print(first_digit_list[0:5])
+
+#variables for matplotlib graph
+#labels = ['G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9']
+#x = np.arange(len(labels))
+#width = 0.35
+
+#fig, ax = plt.subplots()
+##rects1 = ax.bar(x - width/2, first_digit_list, width, label='Census Data')
+#rects2 = ax.bar(x + width/2, benfords_list, width, label='Benford Prediction')
+
+#some text for labels, title, and x-axis tick labels
+# ax.set_ylabel("amount of leading digit occurence")
+# ax.set_title("Benford's assertion within the census data")
+# ax.set_xticks(x)
+# ax.set_xticklabels(labels)
+# ax.legend()
+
+##graphing functions
+
+first_digit_list.sort()
+_=plt.hist(first_digit_list, bins=len(set(first_digit_list)))
+_=plt.xlabel('leading digit')
+_=plt.ylabel('count')
+
+plt.show()
